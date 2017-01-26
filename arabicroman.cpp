acf9bda1a583942358487202ce814a9896415e04
Michael Huynh <huynhm@umich.edu>
2017-01-26 06:04:09 -0500

    testing 4/5, 9,10 and so forth - the major notations

--- arabicroman.cpp
+++ arabicroman.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //divide by 1000
-//divid by 500
+//divide by 500
 //divide by 100
 //divide by 50
 //divide by 10
@@ -18,7 +18,17 @@ int main(){
 
 	arabicToRoman(900);
 	arabicToRoman(1000);
-	//arabicToRoman(1);
+	arabicToRoman(400);
+	arabicToRoman(500);
+	arabicToRoman(90);
+	arabicToRoman(100);
+	arabicToRoman(40);
+	arabicToRoman(50);
+	arabicToRoman(9);
+	arabicToRoman(10);
+	arabicToRoman(4);
+	arabicToRoman(5);
+	arabicToRoman(2);
 
 	return 0;
 }
@@ -29,36 +39,66 @@ void arabicToRoman(int arab){
 		if (arab >= 1000){
 				cout << "M";
 				arab -= 1000;
-				
 		}
 			else{
 				cout << "CM"; arab -= 900;
-			}
-
-
-	}
-
-	/*
-	if(arab < 1){
-		cout << "Undefeined in Roman Numerals" << endl;
-	}
-
-	if( arab >= 1 && arab <= 3){
-
-		while(arab > 0){
-
-			cout  << "I";
-			arab--;
 		}
 
 
-	}else if (arab == 4) {
+	}else if (arab >= 400){
+			if (arab >= 500){
+				cout << "D"; arab -= 500;
 
-		cout <<  "IV";
-	}else if (arab == 5 ){
+			}
+			else {
+				cout << "CD"; arab -= 400;
+			}
 
-		cout << "V";
-	}
-	*/
+		}
+		else if (arab >= 90){
+			if (arab >= 100){
+				cout << "C"; arab -= 100;
+			}
+			else{
+				cout << "XC"; arab -= 90;
+			}
+
+
+		}else if (arab >= 40){
+			if (arab >= 50){
+				cout << "L"; arab -= 50;
+			}
+			else {
+				cout << "XL"; arab -= 40;
+			}
+
+
+		}else if (arab >= 9){
+			if (arab >= 10){
+				cout << "X"; arab -= 10;
+			}
+			else { 
+				cout << "IX"; arab -= 9;
+			}
+
+
+		}else if (arab >= 4){
+			if (arab >= 5){
+				cout << "V"; arab -= 5; 
+			}
+			else {
+				cout << "IV"; arab -= 4;
+			}
+
+
+		}else if (arab >= 1)
+		{
+				cout << "I";
+				--arab;
+		}else {
+
+			cout << "ERROR";
+		}
+
 	cout << endl;
 }
